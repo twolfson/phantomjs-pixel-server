e81b7326685ed92d63dab01c0f2040623ddb412d
Todd Wolfson <todd@twolfson.com>
2013-11-16 14:02:50 -0800

    Wrote out proof of concept for function signature but it is parameters only =(

---
+++ test/function-signature-poc.js
@@ -0,0 +1,12 @@
+var functionSignature = require('function-signature');
+function drawCanvas(cb) {
+  // Draw a white on black checkerboard
+  context.fillStyle = "#000000";
+  context.fillRect(0, 0, 10, 10);
+  context.fillStyle = "#FFFFFF";
+  context.fillRect(0, 0, 5, 5);
+  context.fillRect(5, 5, 5, 5);
+  cb();
+}
+
+console.log(functionSignature(drawCanvas));
