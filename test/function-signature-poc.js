3389d262df17386cbe670938511f1a527f03678c
Todd Wolfson <todd@twolfson.com>
2013-11-16 16:19:26 -0800

    Changes from last time

--- test/function-signature-poc.js
+++ test/function-signature-poc.js
@@ -1,4 +1,40 @@
 var functionSignature = require('function-signature');
+
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+var FN_ARG_SPLIT = /,/;
+var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+// var $injectorMinErr = minErr('$injector');
+function annotate(fn) {
+  var $inject,
+      fnText,
+      argDecl,
+      last;
+
+  if (typeof fn == 'function') {
+    if (!($inject = fn.$inject)) {
+      $inject = [];
+      if (fn.length) {
+        fnText = fn.toString().replace(STRIP_COMMENTS, '');
+        argDecl = fnText.match(FN_ARGS);
+        argDecl[1].split(FN_ARG_SPLIT).forEach(function(arg){
+          arg.replace(FN_ARG, function(all, underscore, name){
+            $inject.push(name);
+          });
+        });
+      }
+      fn.$inject = $inject;
+    }
+  } else if (isArray(fn)) {
+    last = fn.length - 1;
+    assertArgFn(fn[last], 'fn');
+    $inject = fn.slice(0, last);
+  } else {
+    assertArgFn(fn, 'fn', true);
+  }
+  return $inject;
+}
+
 function drawCanvas(cb) {
   // Draw a white on black checkerboard
   context.fillStyle = "#000000";
@@ -9,4 +45,4 @@ function drawCanvas(cb) {
   cb();
 }
 
-console.log(functionSignature(drawCanvas));
+console.log(annotate(drawCanvas));
