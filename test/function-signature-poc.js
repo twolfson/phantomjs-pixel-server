fc11e58fbd4b3bd69946bc836cf04d35cfd533ee
Todd Wolfson <todd@twolfson.com>
2013-11-16 16:20:54 -0800

    Moved to function-to-string. yey

--- test/function-signature-poc.js
+++ test/function-signature-poc.js
@@ -1,39 +1,4 @@
-var functionSignature = require('function-signature');
-
-var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
-var FN_ARG_SPLIT = /,/;
-var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-// var $injectorMinErr = minErr('$injector');
-function annotate(fn) {
-  var $inject,
-      fnText,
-      argDecl,
-      last;
-
-  if (typeof fn == 'function') {
-    if (!($inject = fn.$inject)) {
-      $inject = [];
-      if (fn.length) {
-        fnText = fn.toString().replace(STRIP_COMMENTS, '');
-        argDecl = fnText.match(FN_ARGS);
-        argDecl[1].split(FN_ARG_SPLIT).forEach(function(arg){
-          arg.replace(FN_ARG, function(all, underscore, name){
-            $inject.push(name);
-          });
-        });
-      }
-      fn.$inject = $inject;
-    }
-  } else if (isArray(fn)) {
-    last = fn.length - 1;
-    assertArgFn(fn[last], 'fn');
-    $inject = fn.slice(0, last);
-  } else {
-    assertArgFn(fn, 'fn', true);
-  }
-  return $inject;
-}
+var functionToString = require('function-to-string');
 
 function drawCanvas(cb) {
   // Draw a white on black checkerboard
@@ -45,4 +10,4 @@ function drawCanvas(cb) {
   cb();
 }
 
-console.log(annotate(drawCanvas));
+console.log(functionToString(drawCanvas));
